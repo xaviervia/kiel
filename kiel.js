93bfcd9ecc8df6b31be3c761b41935b2df64ee64
Xavier Via <fernando.via@gmail.com>
2015-04-20 01:02:00 +0200

    Intriguing start

---
+++ kiel.js
@@ -0,0 +1,11 @@
+var Sydney = require("sydney")
+
+var Kiel = function () {}
+
+Kiel.prototype = Object.create(Sydney.prototype)
+
+Kiel.prototype.callback = function () {
+  
+}
+
+module.exports = Kiel
